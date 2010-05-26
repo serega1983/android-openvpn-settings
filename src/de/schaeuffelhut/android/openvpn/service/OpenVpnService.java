0f8f3a260ed77603b6bfe1abb24a1eb45af1e2ce
Friedrich Sch?uffelhut <fries@desert.lnp.org>
2010-05-27 00:41:01 +0200

    Run organize imports.

--- src/de/schaeuffelhut/android/openvpn/service/OpenVpnService.java
+++ src/de/schaeuffelhut/android/openvpn/service/OpenVpnService.java
@@ -25,7 +25,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Binder;
 import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Message;
 import android.preference.PreferenceManager;
