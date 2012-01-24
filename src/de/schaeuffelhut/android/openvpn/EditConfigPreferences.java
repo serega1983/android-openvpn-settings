0516da6eb448f71b2b42993ba486468b283afecf
Friedrich Schaeuffelhut <fries@desert.lnp.org>
2012-01-24 23:46:57 +0100

    Run organize imports.

--- src/de/schaeuffelhut/android/openvpn/EditConfigPreferences.java
+++ src/de/schaeuffelhut/android/openvpn/EditConfigPreferences.java
@@ -3,8 +3,6 @@ package de.schaeuffelhut.android.openvpn;
 import java.io.File;
 import java.util.Arrays;
 
-import org.apache.commons.io.FileUtils;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
