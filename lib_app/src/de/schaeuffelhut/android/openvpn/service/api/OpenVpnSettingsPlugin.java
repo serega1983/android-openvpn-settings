212fac3692204685036c39f44301c4a721994394
Friedrich Schaeuffelhut <fries@desert.lnp.org>
2012-11-22 00:56:35 -0500

    Add class OpenVpnSettingsPlugin.

---
+++ lib_app/src/de/schaeuffelhut/android/openvpn/service/api/OpenVpnSettingsPlugin.java
@@ -0,0 +1,77 @@
+/*
+ * This file is part of OpenVPN-Settings.
+ *
+ * Copyright © 2009-2012  Friedrich Schäuffelhut
+ *
+ * OpenVPN-Settings is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenVPN-Settings is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenVPN-Settings.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Report bugs or new features at: http://code.google.com/p/android-openvpn-settings/
+ * Contact the author at:          android.openvpn@schaeuffelhut.de
+ */
+
+package de.schaeuffelhut.android.openvpn.service.api;
+
+import android.app.PendingIntent;
+import android.content.*;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.*;
+import android.util.Log;
+
+/**
+ * Support plugin registration with OpenVPN Settings.
+ *
+ * @author Friedrich Schäuffelhut
+ * @since 2012-11-20
+ */
+public class OpenVpnSettingsPlugin
+{
+    static final ComponentName COMPONENT_NAME = new ComponentName(
+            "de.schaeuffelhut.android.openvpn",
+            "de.schaeuffelhut.android.openvpn.services.PluginRegistry"
+    );
+
+    private final Context context;
+    private final PackageInfo openVpnSettings;
+
+    public OpenVpnSettingsPlugin(Context context)
+    {
+        this.context = context;
+        openVpnSettings = findOpenVpnSettingsPackage();
+//        Log.d( "OpenVPNSettings", "openVpnSettings.versionCode: " + openVpnSettings.versionCode );
+//        Log.d( "OpenVPNSettings", "openVpnSettings.versionName: " + openVpnSettings.versionName );
+    }
+
+    public boolean isOpenVpnSettingsInstalled()
+    {
+        return openVpnSettings != null;
+    }
+
+    private PackageInfo findOpenVpnSettingsPackage()
+    {
+        for (PackageInfo p : context.getPackageManager().getInstalledPackages( 0 ))
+            if ("de.schaeuffelhut.android.openvpn".equals( p.packageName ))
+                return p;
+        return null;
+    }
+
+    public void showOpenVpnSettingsInMarket()
+    {
+        Intent intent = new Intent( Intent.ACTION_VIEW );
+        intent.setData( Uri.parse( "market://details?id=de.schaeuffelhut.android.openvpn" ) );
+        context.startActivity( intent );
+    }
+}
