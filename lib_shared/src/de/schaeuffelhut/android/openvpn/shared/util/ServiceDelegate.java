2aad228bfd15ebf1e60ded4e63b2a7a1798730de
Friedrich Schaeuffelhut <fries@desert.lnp.org>
2013-03-06 22:31:43 +0100

    Make interface ServiceDelegate public.

--- lib_shared/src/de/schaeuffelhut/android/openvpn/shared/util/ServiceDelegate.java
+++ lib_shared/src/de/schaeuffelhut/android/openvpn/shared/util/ServiceDelegate.java
@@ -26,7 +26,7 @@ package de.schaeuffelhut.android.openvpn.shared.util;
 * @author Friedrich Schäuffelhut
 * @since 2013-03-06
 */
-interface ServiceDelegate
+public interface ServiceDelegate
 {
     public void onCreate();
 
